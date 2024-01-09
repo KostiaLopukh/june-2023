b73a4bdc670a4a48f2349bfa37c7b865dadf9992
feden2906 <feden2906@gmail.com>
2024-01-09 20:50:09 +0200

    hw-2

--- helpers/helper.js
+++
@@ -1,9 +0,0 @@
-function help() {
-  console.log('I am helping you');
-  console.log(__dirname, 'dirname');
-  console.log(__filename, 'filename');
-  console.log(process.cwd(), 'process.cwd');
-
-}
-
-module.exports = { help }
