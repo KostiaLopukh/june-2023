b09c0b7182924443672c44613f9f5a04e0b6e1e1
l4pukh <k0styalopuh3@gmail.com>
2024-02-13 08:38:54 +0200

    sockets

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import fileUpload from "express-fileupload";
+import * as http from "http";
 import * as mongoose from "mongoose";
+import { Server } from "socket.io";
 import * as swaggerUi from "swagger-ui-express";
 
 import { configs } from "./configs/config";
@@ -14,6 +16,39 @@ import { userRouter } from "./routers/user.router";
 import * as swaggerDocument from "./unils/swagger.json";
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, { cors: { origin: "*" } });
+
+// io.use(()=>{
+//
+// })
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("message:create", ({ data }) => {
+    io.emit("all", data);
+  });
+
+  socket.on("click", () => {
+    console.log("click click click ");
+
+    // one to one message
+    // socket.emit("clock");
+
+    // to all users message
+    // io.emit("all", "MESSAGE TO ALL");
+
+    // to all users message except sender
+    // socket.broadcast.emit("allExceptMe", "MESSAGE TO ALL EXCEPT ME");
+  });
+
+  socket.on("room:join", ({ roomId }) => {
+    socket.join(roomId);
+
+    socket.to(roomId).emit("room:newUserJoined", socket.id);
+  });
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,7 +70,7 @@ app.use(
 );
 
 const PORT = configs.PORT;
-app.listen(PORT, async () => {
+server.listen(PORT, async () => {
   await mongoose.connect(configs.DB_URL);
   const user = await userRepository.getOneByParams({});
   if (!user) {
